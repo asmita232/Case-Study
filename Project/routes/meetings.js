7b38474b04311208f3c47fc35cbfba99cd313140
Chavan <Asmita.Chavan@team.telstra.com>
2020-09-15 01:15:54 +0530

    added PATCH request to meeting, handled add_attendee API, execuse_yourself API

--- Project/routes/meetings.js
+++ Project/routes/meetings.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const Router = express.Router()
 const { Meeting } = require('../models/Meeting')
+const { User } = require('../models/User')
 
 Router.get('/', (req, res) => {
+
+    const {date = "All", searchTerm} = req.body
+    
     try {
         const data = Meeting.find()
         .exec((error, result) => {
@@ -37,5 +41,57 @@ Router.post('/', (req, res) => {
     });
 })
 
+Router.patch( '/:id', ( req, res ) => {
+    const id = req.params.id; //meeting id
+    const meeting = req.body;
+    const { action, userEmailId } = req.query
 
+    const attendees = meeting.attendees || [];
+
+    if( !id ) {
+        const err = new Error( 'Id should be provided' );
+        err.status = 400;
+        return res.status(400).send(err.message)
+    }
+
+    if(action === "add_attendee") {
+    
+        Meeting
+            .findByIdAndUpdate( id, { $set: meeting, $addToSet: { attendees: userEmailId } } )
+            .exec(( err, updatedMeeting ) => {
+                if( err ) {
+                    err.status = 500
+                    return res.status(500).send(err.message)
+                    // return next( err );
+                }
+
+                res.status( 200 ).json( updatedMeeting );
+            });
+    }
+
+    else if(action === "excuse_yourself") {
+
+        const userid = req.query.id
+        User.findById(userid,{ emailId : 1, _id : 0}, (error, emailId) => {
+
+            if(error) {
+                return res.status(500).send(err.message)
+            }
+
+            console.log(emailId)
+
+            Meeting.findByIdAndUpdate(id,{
+                $pull: {attendees: emailId.emailId}
+            }, (error, result) => {
+                if(error) {
+                    return res.status(500).send(error.message)
+                }
+                return res.send(result)
+            })   
+        })        
+    }
+    else {
+        res.send('No criteria given!')
+    }
+});
 module.exports = Router

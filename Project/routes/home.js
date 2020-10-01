2d5b947b1008adcee8d44b21526e54e779751c82
GDdaboss <gddaboss@gmail.com>
2020-10-01 14:23:45 +0530

    Added post, put, patch requests on home page

--- Project/routes/home.js
+++ Project/routes/home.js
@@ -5,4 +5,17 @@ Router.get('/', (req, res) => {
     res.send('Home Router')
 })
 
+
+Router.post('/', (req, res) => {
+    res.send('Invalid Post Request')
+})
+
+Router.put('/', (req, res) => {
+    res.send('Invalid Put request')
+})
+
+Router.patch('/', (req, res) => {
+    res.send('Invalid Patch request')
+})
+
 module.exports = Router
